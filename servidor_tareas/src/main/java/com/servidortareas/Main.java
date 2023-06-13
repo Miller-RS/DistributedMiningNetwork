8c891ce31395bd9921ce9e23b2a88fdc0d508687
Gerald Villarroel <gvillaroell@uni.pe>
2023-06-13 13:49:12 -0500

    Change src/Main.java

--- servidor_tareas/src/main/java/com/servidortareas/Main.java
+++ servidor_tareas/src/main/java/com/servidortareas/Main.java
@@ -3,5 +3,6 @@ package com.servidortareas;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! Miller");
+        System.out.println("Hello world! Gerald");
     }
 }
