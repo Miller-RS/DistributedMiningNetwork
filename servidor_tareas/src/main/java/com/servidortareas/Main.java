1137ee6e75ab43321b8b3500eaeac932be36207b
Gerald Villarroel <gvillaroell@uni.pe>
2023-06-14 10:31:36 -0500

    Change src/Main.java from another branch

--- servidor_tareas/src/main/java/com/servidortareas/Main.java
+++ servidor_tareas/src/main/java/com/servidortareas/Main.java
@@ -3,6 +3,6 @@ package com.servidortareas;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! Miller");
-        System.out.println("Hello world! Gerald");
+        System.out.println("Hello world! Gerald from gerald branch");
     }
 }
