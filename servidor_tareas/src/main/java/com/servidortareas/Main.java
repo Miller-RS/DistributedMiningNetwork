96aad8ba3d4c4a1b39f08e5eb65f4dda936de848
Gerald Villarroel <gvillaroell@uni.pe>
2023-06-16 12:25:19 -0500

    Change src/Main.java with my name

--- servidor_tareas/src/main/java/com/servidortareas/Main.java
+++ servidor_tareas/src/main/java/com/servidortareas/Main.java
@@ -3,6 +3,7 @@ package com.servidortareas;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! Miller");
+        System.out.println("Hello world! Gerald");
         System.out.println("Hello world! Miller branch and Gerald branch");
     }
 }
