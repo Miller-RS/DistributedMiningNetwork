7ae7aa8d5f1d916fe1cdd6681b8654f90483e52a
Gerald Villarroel <gvillaroell@uni.pe>
2023-06-16 17:09:56 -0500

    Add list of words and Servidor print the content of that file

--- servidor_tareas/src/main/java/com/servidortareas/Main.java
+++ servidor_tareas/src/main/java/com/servidortareas/Main.java
@@ -1,9 +1,17 @@
 package com.servidortareas;
 
+import com.servidortareas.Servidor;
+
+class GlobalVals {
+    static final String PATH = "E:\\Documents\\Aplicaciones\\Java\\DistributedMiningNetwork\\servidor_tareas\\src\\main\\java\\com\\servidortareas\\listapalabras1.txt";
+}
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! Miller");
         System.out.println("Hello world! Gerald");
         System.out.println("Hello world! Miller branch and Gerald branch");
+        Servidor sr = new Servidor(GlobalVals.PATH);
+        sr.leerArchivo();
     }
 }
--- servidor_tareas/src/main/java/com/servidortareas/Servidor.java
+++ servidor_tareas/src/main/java/com/servidortareas/Servidor.java
@@ -1,5 +1,48 @@
 package com.servidortareas;
 
-public class Servidor {
+import java.io.*;
 
+public class Servidor {
+  private String path;
+
+  public Servidor(String path) {
+    this.path = path;
+  }
+
+  public void leerArchivo() {
+    File archivo = null;
+    FileReader fr = null;
+    BufferedReader br = null;
+
+    try {
+      // Apertura del fichero y creacion de BufferedReader para poder
+      // hacer una lectura comoda (disponer del metodo readLine()).
+      archivo = new File(path);
+      fr = new FileReader(archivo);
+      br = new BufferedReader(fr);
+
+      // Lectura del fichero
+      String linea = br.readLine();
+      while (linea != null) {
+        System.out.println(linea);
+        linea = br.readLine();
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      // En el finally cerramos el fichero, para asegurarnos
+      // que se cierra tanto si todo va bien como si salta
+      // una excepcion.
+      try {
+        if (br != null) {
+          br.close();
+        }
+        if (fr != null) {
+          fr.close();
+        }
+      } catch (Exception e2) {
+        e2.printStackTrace();
+      }
+    }
+  }
 }
