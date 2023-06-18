a0df3d2f57abc4ced14d5c0775b400538b480028
Gerald Villarroel <gvillaroell@uni.pe>
2023-06-18 17:44:33 -0500

    Change the HOST and PORT

--- minero-cliente/Constanst.py
+++ minero-cliente/Constanst.py
@@ -1,5 +1,3 @@
-# HOST = "localhost"
-# PORT = 12345
-HOST = "192.168.18.5"
-PORT = 4444
+HOST = "localhost"
+PORT = 12345
 USER = "John"
--- servidor_tareas/src/main/java/com/servidortareas/VariablesConexion.java
+++ servidor_tareas/src/main/java/com/servidortareas/VariablesConexion.java
@@ -2,6 +2,6 @@ package com.servidortareas;
 
 public class VariablesConexion {
   public final static String PATH = "E:\\Documents\\Aplicaciones\\Java\\DistributedMiningNetwork\\servidor_tareas\\src\\main\\java\\com\\servidortareas\\listapalabras1.txt";
-  public final static int PORT = 4444;
-  public final static String IP = "192.168.18.5";
+  public final static int PORT = 12345;
+  public final static String IP = "localhost";
 }
