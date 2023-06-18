104c1f376589b94390d4fe1a55bf792a6bf3d9ca
Gerald Villarroel <gvillaroell@uni.pe>
2023-06-18 17:54:51 -0500

    Change the Path to a relative Path on VariablesConexion.java

--- servidor_tareas/src/main/java/com/servidortareas/VariablesConexion.java
+++ servidor_tareas/src/main/java/com/servidortareas/VariablesConexion.java
@@ -1,7 +1,17 @@
 package com.servidortareas;
 
+import java.io.File;
+
 public class VariablesConexion {
-  public final static String PATH = "E:\\Documents\\Aplicaciones\\Java\\DistributedMiningNetwork\\servidor_tareas\\src\\main\\java\\com\\servidortareas\\listapalabras1.txt";
+  public final static String PATH = getPathToFile("listapalabras1.txt");
   public final static int PORT = 12345;
   public final static String IP = "localhost";
+
+  private static String getPathToFile(String fileName) {
+    String rootDir = System.getProperty("user.dir");
+    String filePath = rootDir + File.separator + "servidor_tareas" + File.separator + "src" + File.separator + "main"
+        + File.separator + "java" + File.separator + "com" + File.separator + "servidortareas" + File.separator
+        + fileName;
+    return filePath;
+  }
 }
